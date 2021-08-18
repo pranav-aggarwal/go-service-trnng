5271675706f1bb0d53b39b97b57afe2bac060209
Pranav Aggarwal <paggarwal@mast.com>
2021-08-18 20:50:35 +0530

    end to end code

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"flag"
+	"fmt"
+
+	_ "github.com/lib/pq"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+
+	"net/http"
+	"os"
+	"os/signal"
+	"service-trnng/account"
+	"syscall"
+)
+
+const dbSource = "postgres://jddbjrse:gbNgFSw3wOF74Kj1SookyAQ21eJWr81W@chunee.db.elephantsql.com/jddbjrse"
+
+func main() {
+	var httpAdr = flag.String("http", ":8084", "http listen address")
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.NewSyncLogger(logger)
+		logger = log.With(logger,
+			"service", "account",
+			"time: ", log.DefaultTimestampUTC,
+			"caller", log.DefaultCaller,
+		)
+	}
+
+	level.Info(logger).Log("msg", "service started")
+	defer level.Info(logger).Log("msg", "service ended")
+
+	var db *sql.DB
+	{
+		var err error
+		db, err = sql.Open("postgres", dbSource)
+		if err != nil {
+			level.Error(logger).Log("exit", err)
+			os.Exit(-1)
+		}
+	}
+
+	flag.Parse()
+	ctx := context.Background()
+	var srv account.Service
+	{
+		repository := account.NewRepo(db, logger)
+		srv = account.NewService(repository, logger)
+	}
+
+	errs := make(chan error)
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	endpoints := account.MakeEndpoints(srv)
+
+	go func() {
+		fmt.Println("listening on port", *httpAdr)
+		handler := account.NewHTTPServer(ctx, endpoints)
+		errs <- http.ListenAndServe(*httpAdr, handler)
+	}()
+
+	level.Error(logger).Log("exit", <-errs)
+}
